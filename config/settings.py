1e05e33aa871ea4a6cfba2cedd5367cdb84226c7
rakhmatulloazizov <tillo3305@gmail.com>
2023-12-24 23:10:51 +0500

    push on main branch requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -23,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-inhdnvjrghrigkdnjdkkhrg-kgsfnjfhglngmsgg=-wrgnflsgnrlhme/mh;ljjlsmf,,dnkjlkj..mvjkhkkjilhtkdhvldfnd,.kjtkksecure-k-i^w#s4xkt*=f3(6ytume-xpkjlkjlkjlkhjhjkn/./mmlkjlk=8nur0mvhr$1^e=)hk&@yksnh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -82,8 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -103,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -115,12 +109,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'files/static'),
@@ -134,5 +126,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'files/uploads')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+
+try:
+    from .settings_dev import *
+except ImportError:
+    pass
